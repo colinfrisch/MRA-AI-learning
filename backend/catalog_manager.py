163ac0e625de9aebee094f88d0fe0b47abcf9da1
Frisch Colin <colin.frisch@student-cs.fr>
2024-12-25 11:38:26 +0100

    added get_chapter_content in catalog_manager

--- backend/catalog_manager.py
+++ backend/catalog_manager.py
@@ -8,19 +8,19 @@ class CatalogManager():
         pass
 
     def get_chapter_list(self) -> List[str]:
-        with open('../data/chapters.json', 'r') as file:
+        with open('../data/chapters_extended.json', 'r') as file:
             chapters = json.load(file)["chapters"]
 
         return [str(chapter['name'])+' - '+str(chapter['description']) for chapter in chapters]
 
-    def get_chapter_title(self,prompt:str) -> str :
-        pass
-
-    def get_chapter_content(self,prompt:str) -> str : 
-        pass
+    def get_chapter_content(self,chapter_title:str) -> str :
+        with open('../data/chapters_extended.json', 'r') as file:
+            chapters = json.load(file)["chapters"]
+        return [chapter['content'] for chapter in chapters if chapter['name'] == chapter_title][0]
+        
 
     def modify_chapter(self,chapter_title:str,new_chapter_content:str) :
         pass
 
-cat = CatalogManager()
-print(cat.get_chapter_list())
+#cat = CatalogManager()
+#print(cat.get_chapter_list())
