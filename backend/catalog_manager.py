79218eb1afa73121083146150d31b96d6de0ed16
Frisch Colin <colin.frisch@student-cs.fr>
2024-12-25 11:54:09 +0100

    new json

--- backend/catalog_manager.py
+++ backend/catalog_manager.py
@@ -5,19 +5,22 @@ import json
 
 class CatalogManager():
     def __init__(self):
+        with open('../data/chapters_extended.json', 'r') as file:
+            self.chapters = json.load(file)["chapters"]
         pass
 
     def get_chapter_list(self) -> List[str]:
-        with open('../data/chapters_extended.json', 'r') as file:
-            chapters = json.load(file)["chapters"]
-
-        return [str(chapter['name'])+' - '+str(chapter['description']) for chapter in chapters]
+        return [str(chapter['name'])+' - '+str(chapter['description']) for chapter in self.chapters]
 
     def get_chapter_content(self,chapter_title:str) -> str :
-        with open('../data/chapters_extended.json', 'r') as file:
-            chapters = json.load(file)["chapters"]
-        return [chapter['content'] for chapter in chapters if chapter['name'] == chapter_title][0]
+        return [chapter['content'] for chapter in self.chapters if chapter['name'] == chapter_title][0]
         
 
     def modify_chapter(self,chapter_title:str,new_chapter_content:str) :
-        pass
+        [chapter.update({'content':new_chapter_content}) for chapter in self.chapters if chapter['name'] == chapter_title]
+        
+        # Save the new chapters list in test file
+        with open('../data/chapters_extended_test.json', 'w') as file:
+            json.dump({"chapters":self.chapters}, file)
+        
+        return self.chapters
