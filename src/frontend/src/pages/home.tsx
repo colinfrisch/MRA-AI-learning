9fc76d6239c06af60c7267d0b0b2ea9b8277467b
Colin Frisch <colin.frisch@gmail.com>
2025-04-29 18:29:19 +0800

    removed js leftovers

--- src/frontend/src/pages/home.tsx
+++ src/frontend/src/pages/home.tsx
@@ -1,8 +1,11 @@
+import Header from '../components/Header';
 import ChatInterface from '../components/ChatInterface';
 
 export function Home() {
   return (
     <div className="home-page">
+      <Header />
+      <h1>MRA HOME</h1>
       <ChatInterface />
     </div>
   );
